2ab76000d26060773f0dc0a30c7fc808dea98d92
Jonas Kuhn <jonaskuhn220@gmail.com>
2018-10-08 22:56:39 -0300

    dsv-controle-de-materiais-nti Iniciado desenvolvimento em php. Editado nome da classe.

--- index.php
+++ index.php
@@ -29,32 +29,47 @@
                 Faça seu Empréstimo
             </h1>
         </header>
+
         <div class="trava container-fluid mt-md-5">
-            <div class="row item-card text-center">
-                <div class="align-center col-md-3 offset-md-3">
-                    <div class="col align-self-md-center">
-                        <div class="card h-100">
-                            <a href="#" class="hvr-grow-shadow">
-                                <div class="card-body" style="display: flex; align-items: center; justify-content: center; height: 200px;">
-                                    <h3>Alunos</h3>
+            <div class="offset-md-3">
+                <div class="row item-card text-center">
+                    <?php
+                    include './conexao.php';
+                    $sqlTipoPessoa = "SELECT * FROM tb_tipo_pessoa;";
+                    
+                    $sqlTipoPessoaAdm = "SELECT * FROM tb_tipo_pessoa WHERE desc_tipo = 'Administradores';";
+
+                    $queryTipoPessoaAdm = $pdo->query($sqlTipoPessoaAdm);
+
+                    $dadosAdm = $queryTipoPessoaAdm->fetch();
+                    $codAdm = $dadosAdm['cod_tipo_pessoa'];
+                    $descAdm = $dadosAdm['desc_tipo'];
+                    
+                    $queryTipoPessoa = $pdo->query($sqlTipoPessoa);
+
+                    while ($dados = $queryTipoPessoa->fetch()) {
+                        $cod = $dados['cod_tipo_pessoa'];
+                        $desc = $dados['desc_tipo'];
+                        
+                        if($desc != $descAdm){
+                        ?>
+                        <div class="align-center col-md-4">
+                            <div class="col align-self-md-center">
+                                <div class="card h-100">
+                                    <a href="cadastro.php?i=<?= $cod; ?>" class="hvr-grow-shadow">
+                                        <div class="card-body" style="display: flex; align-items: center; justify-content: center; height: 200px;">
+                                            <h3><?= $desc; ?></h3>
+                                        </div>
+                                    </a>
                                 </div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col col-md-3">
-                    <div class="card h-100">
-                        <a href="#" class="hvr-grow-shadow">
-                            <div class="card-body"  style="display: flex; align-items: center; justify-content: center; height: 200px;">
-                                <h3>Professores</h3>
                             </div>
-                        </a>
-                    </div>
+                        </div>
+                    <?php } }?>
                 </div>
             </div>
             <div class="item-card fixed-bottom " >
                 <a href="#" class="hvr-bubble-float-bottom-new" style="float: right; margin-right: 100px; margin-bottom:0px; border-radius: 2px; color: #FFF; padding: 5px;">
-                    Administradores
+                    <?= $descAdm; ?>
                 </a>
             </div>
         </div>
