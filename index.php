b68a4557cdd99255d35efea6c93cf29ffb2cb77a
Jonas Kuhn <jonaskuhn220@gmail.com>
2018-10-09 19:34:56 -0300

    dsv-controle-de-materiais-nti Ajustado tela de login.

--- index.php
+++ index.php
@@ -20,6 +20,7 @@
         <link href="dist/css/bootstrap.css" rel="stylesheet" type="text/css"/>
         <link href="css/estilo.css" rel="stylesheet" type="text/css"/>
         <link href="events/css/hover.css" rel="stylesheet" type="text/css"/>
+        <link href="font-awesome/css/font-awesome.css" rel="stylesheet" type="text/css"/>
     </head>
 
     <body>
@@ -30,49 +31,24 @@
             </h1>
         </header>
 
-        <div class="trava container-fluid mt-md-5">
-            <div class="offset-md-3">
-                <div class="row item-card text-center">
-                    <?php
-                    include './conexao.php';
-                    $sqlTipoPessoa = "SELECT * FROM tb_tipo_pessoa;";
-                    
-                    $sqlTipoPessoaAdm = "SELECT * FROM tb_tipo_pessoa WHERE desc_tipo = 'Administradores';";
+        <?php
+        $url = $_GET['url'];
 
-                    $queryTipoPessoaAdm = $pdo->query($sqlTipoPessoaAdm);
+        switch ($url) {
+            // CASE PARA OS MENUS
+            case 'selecao.php':
+                include './selecao.php';
+                break;
+            case 'administradores.php':
+                include './intranet/login.html';
+                break;
+            default :
+                include './selecao.php';
+                break;
+        }
+        ?>
 
-                    $dadosAdm = $queryTipoPessoaAdm->fetch();
-                    $codAdm = $dadosAdm['cod_tipo_pessoa'];
-                    $descAdm = $dadosAdm['desc_tipo'];
-                    
-                    $queryTipoPessoa = $pdo->query($sqlTipoPessoa);
 
-                    while ($dados = $queryTipoPessoa->fetch()) {
-                        $cod = $dados['cod_tipo_pessoa'];
-                        $desc = $dados['desc_tipo'];
-                        
-                        if($desc != $descAdm){
-                        ?>
-                        <div class="align-center col-md-4">
-                            <div class="col align-self-md-center">
-                                <div class="card h-100">
-                                    <a href="cadastro.php?i=<?= $cod; ?>" class="hvr-grow-shadow">
-                                        <div class="card-body" style="display: flex; align-items: center; justify-content: center; height: 200px;">
-                                            <h3><?= $desc; ?></h3>
-                                        </div>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    <?php } }?>
-                </div>
-            </div>
-            <div class="item-card fixed-bottom " >
-                <a href="#" class="hvr-bubble-float-bottom-new" style="float: right; margin-right: 100px; margin-bottom:0px; border-radius: 2px; color: #FFF; padding: 5px;">
-                    <?= $descAdm; ?>
-                </a>
-            </div>
-        </div>
     </body>
     <script src="dist/js/bootstrap.js" type="text/javascript"></script>
     <!-- Ativar events -->
---
+++ selecao.php
@@ -0,0 +1,46 @@
+<div class="trava container-fluid mt-md-5">
+    <div class="offset-md-3">
+        <div class="row item-card text-center">
+            <?php
+            include './conexao.php';
+            $sqlTipoPessoa = "SELECT * FROM tb_tipo_pessoa;";
+
+            $sqlTipoPessoaAdm = "SELECT * FROM tb_tipo_pessoa WHERE desc_tipo = 'Administradores';";
+
+            $queryTipoPessoaAdm = $pdo->query($sqlTipoPessoaAdm);
+
+            $dadosAdm = $queryTipoPessoaAdm->fetch();
+            $codAdm = $dadosAdm['cod_tipo_pessoa'];
+            $descAdm = $dadosAdm['desc_tipo'];
+
+            $queryTipoPessoa = $pdo->query($sqlTipoPessoa);
+
+            while ($dados = $queryTipoPessoa->fetch()) {
+                $cod = $dados['cod_tipo_pessoa'];
+                $desc = $dados['desc_tipo'];
+
+                if ($desc != $descAdm) {
+                    ?>
+                    <div class="align-center col-md-4">
+                        <div class="col align-self-md-center">
+                            <div class="card h-100">
+                                <a href="cadastro.php?i=<?= $cod; ?>" class="hvr-grow-shadow">
+                                    <div class="card-body" style="display: flex; align-items: center; justify-content: center; height: 200px;">
+                                        <h3><?= $desc; ?></h3>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                <?php
+                }
+            }
+            ?>
+        </div>
+    </div>
+    <div class="item-card fixed-bottom " >
+        <a href="?url=administradores.php" class="hvr-bubble-float-bottom-new" style="float: right; margin-right: 100px; margin-bottom:0px; border-radius: 2px; color: #FFF; padding: 5px;">
+<?= $descAdm; ?>
+        </a>
+    </div>
+</div>
