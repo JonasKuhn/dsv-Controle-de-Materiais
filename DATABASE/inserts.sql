45492cbac3236840a75cc0b5add4e1c9baba4d6b
DESKTOP-EHGFFAH\Emerson <emersonklunk2014@gmail.com>
2018-10-11 13:04:03 -0300

    dsv-controle-de-materiais-nti Realização dos inserts no bd

---
+++ DATABASE/inserts.sql
@@ -0,0 +1,16 @@
+INSERT INTO tb_tipo_equipamento (desc_tipo,qtd_tipo) VALUES ('Filtro de Linha',20);
+INSERT INTO tb_tipo_equipamento (desc_tipo,qtd_tipo) VALUES ('Notebook',48);
+INSERT INTO tb_tipo_equipamento (desc_tipo,qtd_tipo) VALUES ('Passador de Slides',5);
+INSERT INTO tb_tipo_equipamento (desc_tipo,qtd_tipo) VALUES ('Projetor Multimídia',10);
+INSERT INTO tb_tipo_equipamento (desc_tipo,qtd_tipo) VALUES ('Adaptador HDMI',7);
+INSERT INTO tb_tipo_equipamento (desc_tipo,qtd_tipo) VALUES ('T',5);
+INSERT INTO tb_tipo_equipamento (desc_tipo,qtd_tipo) VALUES ('Mouse',5);
+INSERT INTO tb_tipo_equipamento (desc_tipo,qtd_tipo) VALUES ('Teclado Numérico USB',4);
+INSERT INTO tb_tipo_equipamento (desc_tipo,qtd_tipo) VALUES ('Adaptador VGA',2);
+INSERT INTO tb_tipo_equipamento (desc_tipo,qtd_tipo) VALUES ('Leitor de Código de Barras',8);
+
+INSERT INTO tb_admin (nome_admin,senha_admin,login_admin) VALUES ('Administrador','admin','admin');
+
+INSERT INTO tb_tipo_pessoa (desc_tipo) VALUES ('Aluno');
+INSERT INTO tb_tipo_pessoa (desc_tipo) VALUES ('Professor');
+INSERT INTO tb_tipo_pessoa (desc_tipo) VALUES ('Administrador');
