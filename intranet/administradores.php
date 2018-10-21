94d9d0a76e65a7e32e37647f161e8f0bb8df9e7c
Emerson Klunk <emersonklunk2014Q@gmail.com>
2018-10-21 14:42:47 -0300

    dsv-controle-de-materiais-nti Adicionado tela de Gerenciamento de Usuários Administradores e função que torna dinâmico a tela.

---
+++ intranet/administradores.php
@@ -0,0 +1,56 @@
+<style type="text/css">
+    .table-font{
+        font-weight: bold;
+        font-size: 1.3rem;
+    }
+    .table-link{
+        text-decoration: none;
+        color: black;
+    }
+</style>
+<h1 style="margin-top: 3rem">Usuários Administradores</h1>
+<table class="table table-bordered table-hover table-responsive-md" style="font-size: 1.2rem">
+    <thead>
+        <tr>
+            <th scope="col"><a href="?url=usuario-incluir" class="table-link">
+                    <i class="fa fa-plus fa-2x"></i>
+                </a></th>
+            <th scope="col" class="table-font">#</th>
+            <th scope="col" class="table-font">Nome</th>
+            <th scope="col" class="table-font">Login</th>
+            <th scope="col" class="table-font">Senha</th>
+            <th scope="col" class="table-font">Criado</th>
+            <th scope="col" class="table-font">Modificado</th>
+
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        include '../conexao.php';
+        $sql = "select * from tb_admin";
+        $query = $pdo->query($sql);
+        foreach ($query as $row):
+            $id = $row["cod_admin"];
+            $nome = $row["nome_admin"];
+            $login = $row["login_admin"];
+            $senha = $row["senha_admin"];
+            $criado = $row["created"];
+            $modificado = $row["modified"];
+        endforeach;
+        ?>
+        <tr>
+            <td><a href="?url=usuario-alterar&id=<?= $id ?>" class="table-link">
+                    <i class="fa fa-edit fa-2x"></i>
+                </a><br><a href="?url=usuario-excluir&id=<?= $id ?>" onclick="return excluir('<?= $nome ?>');"  class="table-link">
+                    <i class="fa fa-trash-o fa-2x"></i>
+                </a>
+            </td>
+            <td scope="row"><?= $id ?></td>
+            <td><?= $nome ?></td>
+            <td><?= $login ?></td>
+            <td><?= $senha ?></td>
+            <td><?= $criado ?></td>
+            <td><?= $modificado ?></td>
+        </tr>
+    </tbody>
+</table>
--- intranet/intranet.php
+++ intranet/intranet.php
@@ -1,4 +1,4 @@
-<?php require_once 'sessao.php';?>
+<?php require_once 'sessao.php'; ?>
 <html>
     <head>
         <title>Empréstimo NTI</title>
@@ -6,50 +6,66 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
         <link rel="stylesheet" href="assets/css/main.css" />
         <link href="assets/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+        <link href="assets/css/.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
         <!--Sidebar -->
-        <div id="sidebar">
-            <div class="inner">
+        <div class="row">
+            <div class="col-md-3" id="sidebar" style="margin-right: 3rem">
+                <div class="inner">
 
-                <!-- Menu -->
-                <nav id="menu">
-                    <header class="major">
-                        <h2>Empréstimo NTI</h2>
-                    </header>
+                    <!-- Menu -->
+                    <nav id="menu">
+                        <header class="major">
+                            <h2>Empréstimo NTI</h2>
+                        </header>
 
-                    <ul>
-                        <li><a href="intranet.php">Página Inicial</a></li>
-                        <li><a href="#">Cadastro de Equipamento</a></li>
-                        <li><a href="#">Cadastro de Alunos</a></li>
-                        <li>
-                            <span class="opener">Equipamentos</span>
-                            <ul>
-                                <li><a href="#">Notebook</a></li>
-                                <li><a href="#">Régua</a></li>
-                                <li><a href="#">Projetor</a></li>
-                                <li><a href="#">Adaptador HDMI</a></li>
-                                <li><a href="#">Passador de Slides</a></li>
+                        <ul>
+                            <li><a href="intranet.php">Página Inicial</a></li>
+                            <li><a href="intranet.php?url=admin">Administradores</a></li>
+                            <li><a href="#">Cadastro de Equipamento</a></li>
+                            <li><a href="#">Cadastro de Alunos</a></li>
+                            <li>
+                                <span class="opener">Equipamentos</span>
+                                <ul>
+                                    <li><a href="#">Notebook</a></li>
+                                    <li><a href="#">Régua</a></li>
+                                    <li><a href="#">Projetor</a></li>
+                                    <li><a href="#">Adaptador HDMI</a></li>
+                                    <li><a href="#">Passador de Slides</a></li>
 
-                            </ul>
-                        </li>
-                        <li><a href="logout.php">Sair</a></li>
-                    </ul>
-                </nav>
+                                </ul>
+                            </li>
+                            <li><a href="logout.php">Sair</a></li>
+                        </ul>
+                    </nav>
 
-                <!--Footer--> 
-                <footer id="footer">
-                    <p class="copyright">&copy; Leonardo Roden. All rights reserved.</p>
-                </footer>
+                    <!--Footer--> 
+                    <footer id="footer">
+                        <p class="copyright">&copy; Leonardo Roden. All rights reserved.</p>
+                    </footer>
+                </div>
             </div>
+            <div class="col-md-8">
+                <?php
+                $url = $_GET['url'];
+                switch ($url) {
+                    case'admin':
+                        include('administradores.php');
+                        break;
+                    default :
+                }
+                ?>
+            </div>
+            <div class="trava"></div>
         </div>
 
 
-    <!-- Scripts -->
-    <script src="assets/js/jquery.min.js"></script>
-    <script src="assets/js/skel.min.js"></script>
-    <script src="assets/js/util.js"></script>
-    <script src="assets/js/main.js"></script>
+        <!-- Scripts -->
+        <script src="assets/js/jquery.min.js"></script>
+        <script src="assets/js/skel.min.js"></script>
+        <script src="assets/js/util.js"></script>
+        <script src="assets/js/main.js"></script>
 
-</body>
+    </body>
 </html>
