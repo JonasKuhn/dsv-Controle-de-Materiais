f7b51230ae0e4154fa6ea119c7ed68f51848b48e
Emerson Klunk <emersonklunk2014@gmail.com>
2018-10-30 16:54:41 -0300

    dsv-controle-de-materiais-nti removido link de código js redundante

--- intranet/equipamentos.php
+++ intranet/equipamentos.php
@@ -105,8 +105,5 @@ if (isset($msg) && $msg != false && $msg == "exc") {
         </tr>
     </tbody>
 </table>
-<script type="text/javascript" src="https://cdn.datatables.net/v/bs4/jq-3.3.1/dt-1.10.18/datatables.min.js"></script>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
+
 
